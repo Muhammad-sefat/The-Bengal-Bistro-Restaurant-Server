6171cc687c993b9ff2d37ce2d988f5783f9e01be
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-05-28 22:10:57 +0600

    Create Json toke and Verify

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
+var cookieParser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 
 // middlewere
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+
+// VerifyToken
+const verifyToken = (req, res, next) => {
+  console.log("Inside Token", req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "Unauthorized Access!" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized Access!" });
+    }
+    res.decoded = decoded;
+    next();
+  });
+};
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dbn21dt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,6 +49,17 @@ async function run() {
     const reviewCollection = client.db("coffeeDB").collection("review");
     const cartCollection = client.db("coffeeDB").collection("carts");
     const usersCollection = client.db("coffeeDB").collection("users");
+
+    // Json token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "3h",
+      });
+      res.send({ token });
+    });
+
+    // MENU RELETED API
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -73,7 +104,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -85,7 +116,7 @@ async function run() {
       res.send(result);
     });
 
-    app.patch("/user/admin/:id", async (req, res) => {
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
@@ -95,6 +126,20 @@ async function run() {
       };
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
+    });
+
+    app.get("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(401).send({ message: "Unauthorized Access!" });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
