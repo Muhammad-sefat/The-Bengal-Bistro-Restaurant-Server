fbf0645495ed276660d0d8ecd3c3015cf137a8bb
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-05-28 20:11:36 +0600

    Complete update admin role

--- index.js
+++ index.js
@@ -65,12 +65,35 @@ async function run() {
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
-      console.log(query);
       const ExistingUser = await usersCollection.findOne(query);
       if (ExistingUser) {
         return res.send({ message: "User Already Exists", insertedId: null });
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/user/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
